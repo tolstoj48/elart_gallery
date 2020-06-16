98e8bbbc47139df2bbfdad5ea07da656aa6b194d
Musil Petr <Petr.Musil@agentura-api.org>
2020-06-16 23:38:53 +0200

    Added JS for detail views of particular images and control with navigation on detail view.

--- js/galerie.js
+++ js/galerie.js
@@ -34,24 +34,80 @@
     showHideImages();
   }
 
+  // Returns the index of image node in the passed nodelist
+  function getIndexOfImg(image, array) {
+    for (let i = 0; i < array.length; i ++) {
+      if (array[i].alt == image.alt) {
+        return i;
+      }
+    }
+  }
+
+  function switchImg(where) {
+    let arrayAllImgs = document.querySelectorAll(".show img");
+    let toBeReplacedImg = document.querySelector("#detail_img img");
+    let index = getIndexOfImg(toBeReplacedImg, arrayAllImgs);
+    if (where == "back") {
+      if (index > 0) {
+        let newImg = arrayAllImgs[index - 1].cloneNode(true);
+        toBeReplacedImg.parentNode.replaceChild(newImg, toBeReplacedImg);
+        document.querySelector("#detail_img img").classList.add("window_gallery_img");
+        document.querySelector(".info").innerHTML = index + " / " + arrayAllImgs.length;
+      };
+    } else {
+      if (index < arrayAllImgs.length - 1) {
+        let newImg = arrayAllImgs[index + 1].cloneNode(true);
+        toBeReplacedImg.parentNode.replaceChild(newImg, toBeReplacedImg);
+        document.querySelector("#detail_img img").classList.add("window_gallery_img");
+        document.querySelector(".info").innerHTML = (index + 2) + " / " + arrayAllImgs.length;
+      }
+    }
+  }
+
+  function addControlsGallery(wrapper, imgForGettingNumber) {
+    let containerControls = document.createElement("div");
+    let myButtonBack = document.createElement("div");
+    let numberOfImg = document.createElement("div");
+    let myButtonForward = document.createElement("div");
+    let arrayAllImgs = document.querySelectorAll(".show img");
+    let positionOfImg;
+    myButtonBack.innerHTML = "<";
+    myButtonBack.classList.add("window_gallery_btn");
+    myButtonBack.classList.add("pointer");
+    myButtonBack.addEventListener("click", function () {switchImg("back")});
+    containerControls.appendChild(myButtonBack);
+    positionOfImg = getIndexOfImg(imgForGettingNumber, arrayAllImgs);
+    numberOfImg.innerHTML = (positionOfImg + 1) + " / " + arrayAllImgs.length;
+    numberOfImg.classList.add("window_gallery_btn", "info");
+    containerControls.appendChild(numberOfImg);
+    myButtonForward.innerHTML = ">";
+    myButtonForward.classList.add("window_gallery_btn");
+    myButtonForward.classList.add("pointer");
+    myButtonForward.addEventListener("click", function () {switchImg("forward")});
+    containerControls.appendChild(myButtonForward);
+    wrapper.appendChild(containerControls);
+  }
+
   function toggleDetail() {
     if (window.matchMedia("(min-width: 600px)").matches) {
-    let on = document.getElementById("detail");
+    let detailImg = document.getElementById("detail");
     let wrapper = document.getElementById("detail_img");
-    if (on.style.display == "none") {
+    if (!detailImg.classList.contains("active")) {
       let clone = this.childNodes[1].cloneNode(true);
-      on.style.display = "block";
       clone.style.maxWidth = "80%";
       clone.classList.add("window_gallery_img");
       wrapper.appendChild(clone);
+      detailImg.classList.add("active")
+      addControlsGallery(wrapper, this.childNodes[1]);
     } else {
-      on.style.display = "none";
+      while(wrapper.firstChild) {
       wrapper.removeChild(wrapper.lastChild);
+      };
+      detailImg.classList.remove("active");
     }
   }
   }
   
-  document.getElementById("detail").style.display = "none";
   document.getElementById("close").addEventListener("click", toggleDetail);
 
   for (let i = 0; i < document.getElementsByClassName("btn_galerie_filter").length; i++) {
@@ -65,6 +121,4 @@
     let imgContainer = imgContainerArr[i];
     imgContainer.addEventListener("click", toggleDetail);
   }
-  
-
 })();
